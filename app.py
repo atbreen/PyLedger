3765c79a80e074f71ea25606997019848584ce63
ATBreen <84913184+atbreen@users.noreply.github.com>
2023-11-09 17:05:22 -0500

    streamlit tests

--- app.py
+++ app.py
@@ -1,7 +1,29 @@
-from flask import Flask
+# pylint: skip-file
 
-app = Flask(__name__)
+import streamlit as st
+import pandas as pd
+import matplotlib.pyplot as plt
+# Title of the web app 
+st.title('Hello World!') 
+# Input text box 
+user_input = st.text_input('Enter your name') 
+# Display the input 
+st.write(f'Hello, {user_input}!')
 
-@app.route("/")
-def hello_world():
-    return "<p>Hello, World!</p>"
+@st.cache_data
+def load_data(file):
+    data = pd.read_csv(file)
+    return data
+
+st.title('Simple Data Application')
+uploaded_file = st.file_uploader("Choose a CSV file", type="csv")
+
+if uploaded_file is not None:
+    data = load_data(uploaded_file)
+    st.write(data.head(20))
+
+if uploaded_file is not None:
+    if st.checkbox('Show histogram'): 
+        column = st.selectbox('Select the column to create a histogram', data.columns) 
+        plt.hist(data[column].dropna()) 
+        st.pyplot(plt)
