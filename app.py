84d3014a4afb73d936bb770ad03c79d9d6e5e8ac
aprilBreen <84913184+atbreen@users.noreply.github.com>
2023-11-30 21:01:36 -0500

    fix import issue

--- app.py
+++ app.py
@@ -1,7 +1,5 @@
 import streamlit as st
 import pandas as pd
-import matplotlib.pyplot as plt
-import plotly.express as px
 import utils as u
 from game import Game
 from game_library import GameLibrary
